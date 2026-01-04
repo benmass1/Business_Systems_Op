21e3ebb692cdf4dca9a2dd9abdcfe5d5055c6612
benmass1 <ssalawe70@gmail.com>
2026-01-04 10:48:51 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Kusajili kurasa (Blueprints)
+    from app.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    
+    from app.sales import sales as sales_blueprint
+    app.register_blueprint(sales_blueprint)
+
+    return app
 
