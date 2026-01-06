e50120ec3e8ab098fc30fa93945ba6be557ef4a7
benmass1 <ssalawe70@gmail.com>
2026-01-06 15:34:38 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = "main.login"
 
 def create_app():
     app = Flask(__name__)
-
-    app.config["SECRET_KEY"] = "business_systems_secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///business.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.init_app(app)
-    login_manager.init_app(app)
+    app.secret_key = "business_secret_2026"
 
     from app.routes import main
     app.register_blueprint(main)
-
-    with app.app_context():
-        db.create_all()
 
     return app
