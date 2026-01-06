eb097a7d97cebaf1ba21f756a2de786ab0ac1600
benmass1 <ssalawe70@gmail.com>
2026-01-06 14:59:07 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), default="admin")
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
