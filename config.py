993776b1e5da32dbb17033ee154c464f06bf3cfe
benmass1 <ssalawe70@gmail.com>
2026-01-06 04:59:17 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'funguo-ya-siri-ya-masanja'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../data/business.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
