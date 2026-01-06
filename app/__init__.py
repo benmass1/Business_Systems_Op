18323b3180c3321472284455aba6f282513e4b86
benmass1 <ssalawe70@gmail.com>
2026-01-06 23:58:39 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,35 +6,35 @@ import os
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = "main.login"
-login_manager.login_message_category = "info"
-
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = os.environ.get(
-        "SECRET_KEY", "business_secret_2026"
-    )
+    app.config["SECRET_KEY"] = "business_secret_2026"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///business.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    # init extensions
     db.init_app(app)
     login_manager.init_app(app)
 
-    # import models
     from app.models import User
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # register routes (blueprint)
     from app.routes import main
     app.register_blueprint(main)
 
-    # create tables
     with app.app_context():
         db.create_all()
+
+        # CREATE ADMIN ONCE
+        admin = User.query.filter_by(username="admin").first()
+        if not admin:
+            admin = User(username="admin", is_admin=True)
+            admin.set_password("admin123")
+            db.session.add(admin)
+            db.session.commit()
 
     return app
