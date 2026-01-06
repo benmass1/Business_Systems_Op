ece5b84b88279c1cecac2c76d65e848d54c78f58
benmass1 <ssalawe70@gmail.com>
2026-01-06 07:00:10 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,24 +1,7 @@
-from app import db
 from flask_login import UserMixin
-from datetime import datetime
+from app import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), default='staff') # admin au staff
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    buying_price = db.Column(db.Float, nullable=False)
-    selling_price = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Integer, default=0)
-
-class Sale(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
-    quantity = db.Column(db.Integer, nullable=False)
-    total_price = db.Column(db.Float, nullable=False)
-    date_sold = db.Column(db.DateTime, default=datetime.utcnow)
-
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
